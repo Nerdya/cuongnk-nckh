d6793bb18152823192f6fe42fe66dedf2180cb50
cuongnk <cuongnk@thinhtientech.com>
2023-01-30 16:04:12 +0700

    Setup routes

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 
 
+
+const routes: Routes = [
+  { path: '',  component: UserComponent }
+];
 
 @NgModule({
   declarations: [
     UserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class UserModule { }
