970841ceb84ebf61bc6d4009862fe713255aca6b
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2023-02-03 23:09:09 +0700

    Rerouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
   { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
-  { path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule) },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: '', pathMatch: 'full', redirectTo: 'admin' }
+  { path: '', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule) },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
