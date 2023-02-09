41ce6491c86d2749315941057473e34ec202cb11
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2023-02-09 22:34:27 +0700

    Implements HTTP requests

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 
 @NgModule({
@@ -16,10 +18,14 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
   exports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     IconsProviderModule,
     NzLayoutModule,
     NzMenuModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
   ]
 })
 export class SharedModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+import { appConfig } from './app-config';
+
+export const environment = {
+  production: false,
+  API_URL: appConfig.API_URL,
+  // authenticate
+  LOGIN: '/authenticate/login',
+  REGISTER: '/authenticate/register',
+  
+};
